4b440c630f90243edc058b2efbaf3277f33a490e
Chi-Wei <joeyin@good-series.com>
2024-08-16 11:14:08 -0400

    Add role checker when update about

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\AboutUpdateRequest;
 use App\Models\About;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
 
 class AboutController extends Controller
 {
@@ -20,6 +21,10 @@ class AboutController extends Controller
   public function update(AboutUpdateRequest $request): JsonResponse
   {
     $request->validated();
+
+    if (Auth::user()->role !== 'Admin') {
+      return response()->json([], 404);
+    }
 
     try {
       About::where('id', 1)->update(['content' => $request->content]);
